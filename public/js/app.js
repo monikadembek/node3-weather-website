97bda8f053fd67ee3eced101a2e00427ce3c7039
Monika Dembek <monika.dembek@gmail.com>
2021-04-24 15:04:18 +0200

    remove unnecessary code

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,4 @@
 console.log('Client side javascript files is loaded');
-// fetch - browser based api, to fetch data from server
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//   response.json().then((data) => {
-//     console.log(data);
-//   });
-// });
-
-
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
